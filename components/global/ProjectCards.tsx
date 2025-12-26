76ee51da0aeffd71cc879add97a41fba4f8cef77
Daksh Singh <daksh.singh.2407@gmail.com>
2025-12-26 22:43:58 +0530

    Added proper description of the projects

--- components/global/ProjectCards.tsx
+++ components/global/ProjectCards.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { classNames } from "../styles";
 
 interface CardProps {
     image: string;
@@ -10,26 +9,30 @@ interface CardProps {
 
 export default function ProjectCards({ image, text, paragraph, details }: CardProps ) {
     return (
-        <div className={`${classNames.card} w-full max-w-[80%] mx-auto h-87.5 cursor-default flex flex-col md:flex-row`}>
+        <div className="w-full max-w-[80%] mx-auto my-4 cursor-default flex flex-col md:flex-row border border-gray-700/50 shadow-lg rounded-lg overflow-hidden transition-shadow duration-300 ease-in-out hover:shadow-[0_0_20px_rgba(139,92,246,0.5)]">
             <div className="p-4 md:w-1/2 flex flex-col justify-center">
                 <h1 className="text-center mb-4 text-xl">{text}</h1>
                 <hr className="mt-4 mb-4" />
-                <p className="text-center text-wrap">{paragraph}</p>
+                <p className="text-center text-wrap">{paragraph}</p>{" "}
+                <hr className="mt-2 mb-2" />
                 <ul>
                     {details.map((detail, index) => (
-                        <li key={index} className="text-center text-wrap list-disc list-inside">
+                        <li
+                            key={index}
+                            className="text-center text-wrap list-disc list-inside"
+                        >
                             {detail}
                         </li>
                     ))}
                 </ul>
             </div>
-            <div className="md:w-1/2">
+            <div className="md:w-1/2 min-h-0">
                 <Image
                     src={image}
                     width={350}
                     height={350}
                     alt={text}
-                    className="w-full h-full object-cover"
+                    className="w-full h-64 md:h-full object-cover"
                     loading="eager"
                 />
             </div>

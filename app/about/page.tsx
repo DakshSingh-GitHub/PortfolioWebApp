85bc94fcf9dda32d83fdbeba066deb95addc1f45
Daksh Singh <daksh.singh.2407@gmail.com>
2025-12-27 22:39:46 +0530

    Added links, at the top section of the page of About.tsx, for easy reach.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { classNames } from "@/components/styles";
 import Card from "@/components/global/Card";
 
+import { FaGithub, FaLinkedin, FaInstagram, FaWhatsapp, FaDiscord } from "react-icons/fa";
+
 const DescriptionString = {
     school: "Secondary and Senior Secondary Education from Shri Gulab Rai Montessori school",
 };
@@ -15,6 +17,29 @@ const schoolList = [
         href: "https://grmschool.com/Default.aspx",
         key: 0,
     },
+];
+
+const socialLinks = [
+    {
+        href: "https://github.com/DakshSingh-GitHub",
+        icon: <FaGithub size={24} />,
+    },
+    {
+        href: "https://www.linkedin.com/in/daksh-singh-116566331/",
+        icon: <FaLinkedin size={24} />,
+    },
+    {
+        href: "https://www.instagram.com/dtlz_564",
+        icon: <FaInstagram size={24} />,
+    },
+    {
+        href: "https://wa.me/+919045390564",
+        icon: <FaWhatsapp size={24} />,
+    },
+    {
+        href: "https://discord.com/users/daksh_dtlz_465",
+        icon: <FaDiscord size={24} />,
+    }
 ];
 
 export default function AboutPage() {
@@ -31,7 +56,7 @@ export default function AboutPage() {
     ));
 
     return (
-        <div className="p-10">
+        <div className="p-10 cursor-default">
             <div className="toplevel flex flex-col items-center w-3/4 mx-auto pb-10">
                 <h1
                     className={`${classNames.aboutHeader} ${classNames.textGlowEffect}`}
@@ -43,23 +68,44 @@ export default function AboutPage() {
                     opportunity to share a bit about my educational background.
                     I completed both my Secondary (10th Grade) and Senior
                     Secondary (12th Grade) education at&nbsp;
-                    <span className={`${classNames.textGlowEffect} ${classNames.spanText}`}>Shri Gulab Rai Montessori School</span>, which is affiliated
-                    with the CBSE Board. My time at this institution has been
-                    instrumental in shaping my academic journey and personal
-                    growth. I am proud to have achieved commendable scores in
-                    both examinations, reflecting my dedication and hard work
-                    throughout these formative years. As I continue to pursue my
-                    interests and career goals, I carry forward the values and
-                    knowledge imparted to me during my schooling. Since the
-                    childhood, I have been deeply passionate about technology
-                    and I&apos;ve started to learn about them at a young age.
-                    This passion has driven me to explore various facets of the
-                    tech world, from software development to understanding the
-                    latest advancements in technology. I am excited to continue
-                    this journey, leveraging my educational foundation to
-                    further my skills and contribute meaningfully to the tech
-                    community.
+                    <span
+                        className={`${classNames.textGlowEffect} ${classNames.spanText}`}
+                    >
+                        Shri Gulab Rai Montessori School
+                    </span>
+                    , which is affiliated with the CBSE Board. My time at this
+                    institution has been instrumental in shaping my academic
+                    journey and personal growth. I am proud to have achieved
+                    commendable scores in both examinations, reflecting my
+                    dedication and hard work throughout these formative years.
+                    As I continue to pursue my interests and career goals, I
+                    carry forward the values and knowledge imparted to me during
+                    my schooling. Since the childhood, I have been deeply
+                    passionate about technology and I&apos;ve started to learn
+                    about them at a young age. This passion has driven me to
+                    explore various facets of the tech world, from software
+                    development to understanding the latest advancements in
+                    technology. I am excited to continue this journey,
+                    leveraging my educational foundation to further my skills
+                    and contribute meaningfully to the tech community.
                 </p>
+                <div className="linkbox border-2 px-10 py-4 rounded-4xl  transition-shadow duration-300 ease-in-out hover:shadow-[0_0_45px_rgba(76,5,119,1)] mt-5 hover:border-purple-500">
+                    <h1 className="text-xl text-center">Reach me out at these handles</h1>{" "}
+                    <br />
+                    <div className="flex gap-15">
+                        {socialLinks.map((link) => (
+                            <Link
+                                key={link.href}
+                                href={link.href}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className={`hover:text-gray-300 ${classNames.textGlowEffect}`}
+                            >
+                                {link.icon}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
             </div>
             <hr />
             <div className="schoolList mt-10">
--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaInstagram, FaWhatsapp, FaDiscord} from "react-icons/fa";
 
 export default function Footer() {
     const textGlowEffect =
@@ -18,10 +18,18 @@ export default function Footer() {
             href: "https://www.instagram.com/dtlz_564",
             icon: <FaInstagram size={24} />,
         },
+        {
+            href: "https://wa.me/+919045390564",
+            icon: <FaWhatsapp size={24} />,
+        },
+        {
+            href: "https://discord.com/users/daksh_dtlz_465",
+            icon: <FaDiscord size={24} />,
+        }
     ];
 
     return (
-        <footer className="bg-black/50 text-white backdrop-blur-sm border-t border-gray-700/50 transition-shadow duration-300 ease-in-out hover:shadow-[0_-10px_25px_-5px_rgba(76,5,119,0.4)] mt-10">
+        <footer className="bg-black/50 text-white backdrop-blur-sm border-t border-gray-700/50 transition-shadow duration-300 ease-in-out hover:shadow-[0_-10px_25px_-5px_rgba(76,5,119,0.8)] mt-10 hover:border-purple-500">
             <div className="container mx-auto px-6 py-8">
                 <div className="flex flex-col md:flex-row justify-between items-center">
                     <div className="mb-4 md:mb-0">

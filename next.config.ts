4562ecc047c3933dcd75bc18888cab70e8a3cbe3
Daksh Singh <daksh.singh.2407@gmail.com>
2025-12-29 14:00:42 +0530

    Fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.DEPLOY_TARGET === "gh-pages";
+const repo = "portfolio";
+
 const nextConfig: NextConfig = {
     output: "export",
     distDir: "dist",
     images: { unoptimized: true },
+    basePath:
+        isGithubPages && process.env.NODE_ENV === "production"
+            ? `/${repo}`
+            : "",
+    assetPrefix:
+        isGithubPages && process.env.NODE_ENV === "production"
+            ? `/${repo}/`
+            : "",
 };
 
 export default nextConfig;
